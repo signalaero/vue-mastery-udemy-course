e92e5a4d9725d05fb0c36278dcc82b67e243e64f
Nick Weiner <signalrorak@gmail.com>
2022-06-15 16:22:53 -0400

    update 37 - starter

--- app.js
+++ app.js
@@ -1,43 +1,7 @@
-Vue.createApp({
-    data() {
-        return {
-            perspective: 100,
-            rotateX: 0,
-            rotateY: 0,
-            rotateZ: 0
-        }
-    },
-    computed: {
-        box() {
-            return {
-                transform: `
-                    perspective(${this.perspective}px)
-                    rotateX(${this.rotateX}deg)
-                    rotateY(${this.rotateY}deg)
-                    rotateZ(${this.rotateZ}deg)
-                    `
-            }
-        }
-    },
-    methods: {
-        reset() {
-            this.perspective = 100
-            this.rotateX = 0
-            this.rotateY = 0
-            this.rotateZ = 0
-        },
-        copy() {
-            const el = document.createElement('textarea')
-
-            el.setAttribute('readonly', '')
-            el.style.position = 'absolute'
-            el.style.left = '-9999px'
-            el.value = `transform: ${this.box.transform}`
-
-            document.body.appendChild(el)
-            el.select()
-            navigator.clipboard.writeText(this.box.transform)
-            document.body.removeChild(el)
-        }
+let vm = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!"
     }
+  }
 }).mount('#app')
