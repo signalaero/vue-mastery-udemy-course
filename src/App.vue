bb055e3143a3450a17c555bc73f2fc3a07af1d35
Nick Weiner <signalrorak@gmail.com>
2022-06-21 16:05:32 -0400

    update 73 - dynamic components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,27 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <select v-model="componentName">
+    <option value="HomePage">Home</option>
+    <option value="AboutPage">About</option>
+  </select>
+
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue';
+import AboutPage from './components/AboutPage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HomePage, AboutPage,
+  },
+  data() {
+    return {
+      componentName: "HomePage",
+    };
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
