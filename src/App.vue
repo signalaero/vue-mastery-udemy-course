0e065ee0500b5043faf67378baa5acaa8b71cf24
Nick Weiner <signalrorak@gmail.com>
2022-06-21 23:08:43 -0400

    update 79 - CSS and JavaScript Transitions

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,8 @@
    @before-leave="beforeLeave"
    @leave="leave"
    @after-leave="afterLeave"
-   :css="false"
+   :css="true"
+   name="fade"
   >
     <h2 v-if="flag">Hey</h2>
   </transition>
@@ -37,16 +38,16 @@ export default {
     beforeEnter(el) {
       console.log("before-enter event fired", el);
     },
-    enter(el, done) {
+    enter(el) {
       console.log("enter event fired", el);
 
-      const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
-        duration: 1000, 
-      });
+      // const animation = el.animate([{ transform: "scale3d(0,0,0)" }, {}], {
+      //   duration: 1000, 
+      // });
 
-      animation.onfinish = () => {
-        done();
-      }
+      // animation.onfinish = () => {
+      //   done();
+      // };
     },
     afterEnter(el) {
       console.log("after-enter event fired", el);
@@ -54,14 +55,16 @@ export default {
    beforeLeave(el) {
       console.log("before-leave event fired", el);
    },
-   leave(el, done) {
-      const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
-        duration: 1000, 
-      });
+   leave(el) {
 
-      animation.onfinish = () => {
-        done();
-      }
+      console.log("leave event fired", el);
+      // const animation = el.animate([{}, { transform: "scale3d(0,0,0)" }], {
+      //   duration: 1000, 
+      // });
+
+      // animation.onfinish = () => {
+      //   done();
+      // };
    },
    afterLeave(el) {
       console.log("afterLeave event fired", el);
