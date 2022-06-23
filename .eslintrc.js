709ccefd27f211e4e9eb43b3c03c27465ed3cb9e
Nick Weiner <signalrorak@gmail.com>
2022-06-23 00:19:15 -0400

    fixed broken template

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,19 @@
 module.exports = {
   root: true,
-
   env: {
     node: true,
   },
-
   extends: [
     'plugin:vue/vue3-essential',
     '@vue/airbnb',
   ],
-
   parserOptions: {
     parser: '@babel/eslint-parser',
   },
-
   rules: {
-    'no-console': 'off',
-    'no-debugger': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
-
   overrides: [
     {
       files: [
@@ -30,9 +25,4 @@ module.exports = {
       },
     },
   ],
-
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/airbnb'
-  ]
 };
